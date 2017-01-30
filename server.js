c648f5d56c1f4b3c1366f9193d21ac207eba685c
Samuele Veneruso <samuele.veneruso@gmail.com>
2017-01-30 18:26:33 +0100

    Changed default port for start script to 9001

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/build'));
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 9001);
 
 
